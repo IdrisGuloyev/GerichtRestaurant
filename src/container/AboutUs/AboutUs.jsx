aa8df7bd88c001eeff545b37ca86aed20f269871
IdrisGuloyev <idrisguloyev@gmail.com>
2023-01-12 00:17:36 +0600

    Added Wine and Beer, Cocktails items

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -26,7 +26,7 @@ const AboutUs = () => (
         <h1 className='headtext__cormorant'>Our History</h1>
         <img src={images.spoon} alt='about_spoon' className='spoon__img'/>
         <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
-        <button type='button' className='custom__button'>Knew More</button>
+        <button type='button' className='custom__button'>Know More</button>
       </div>
 
     </div>
